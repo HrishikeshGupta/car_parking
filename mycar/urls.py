9f09c510d876d179a6b830dad70996c5b3f205f6
hrishi <hrishi@HRISHI-UBUNTU>
2020-04-01 15:58:12 +0530

    ENHANCED UI, ADDED GRAPHS, ADDED CHECK

--- mycar/urls.py
+++ mycar/urls.py
@@ -21,7 +21,13 @@ from django.contrib import admin
 # ]
 
 
-from django.urls import include, path
+# from django.urls import path
+
+from django.contrib import admin
+
+from django.conf.urls import url
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('cars/', include('cars.urls')),
